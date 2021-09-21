89475e86ac108a684e1b8b5dd6b49562a0b1e0cf
Ploo Jompong <50630228+ponjom2@users.noreply.github.com>
2021-09-21 17:24:01 +0200

    New function & style

--- js/app.js
+++ js/app.js
@@ -3,10 +3,12 @@ const input = document.getElementById("input");
 const addBtn = document.getElementById("add-button");
 const todoList = document.getElementById("todo-list");
 const removeBtn = document.getElementById("remove-button")
+const angryFace = document.getElementById("angry-face");
 let buttonCreated = false;
 
-// Events
+// EventListeners
 addBtn.addEventListener("click", newTodo);
+angryFace.addEventListener("mouseenter", gladFace);
 
 // Functions for EventListener
 function newTodo(e) {
@@ -36,4 +38,14 @@ function newButton() {
   button.setAttribute("class", "btn btn-width");
   button.appendChild(i);
   i.setAttribute("class", "fas fa-minus-square");
+}
+
+function gladFace() {
+  if (!angryFace.hasAttribute("fa-angry")) {
+    angryFace.classList.remove("fa-angry");
+    angryFace.classList.add("fa-smile-beam");
+  } else {
+    angryFace.classList.remove("fa-smile-beam");
+    angryFace.classList.add("fa-angry");
+  }
 }
