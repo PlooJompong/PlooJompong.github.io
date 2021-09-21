cae02e9965885f6d73847aa3ca2834a810dc382e
Ploo Jompong <50630228+ponjom2@users.noreply.github.com>
2021-09-21 18:41:25 +0200

    New design
    delete remove button function in html && js

--- js/app.js
+++ js/app.js
@@ -4,7 +4,6 @@ const addBtn = document.getElementById("add-button");
 const todoList = document.getElementById("todo-list");
 const removeBtn = document.getElementById("remove-button")
 const angryFace = document.getElementById("angry-face");
-let buttonCreated = false;
 
 // EventListeners
 addBtn.addEventListener("click", newTodo);
@@ -15,30 +14,29 @@ function newTodo(e) {
   e.preventDefault();
 
   // Add listitem
-  const listItem = document.createElement("li");
+  const li = document.createElement("li");
+  const span = document.createElement("span");
+  const div = document.createElement("div");
+  const icon = document.createElement("i");
   if (input.value != "") {
-    listItem.innerText = input.value;
-    todoList.appendChild(listItem);
-    listItem.setAttribute("class", "task");
+    li.innerText = input.value;
 
-    // Check if remove button is created 
-    if (!buttonCreated) {
-      newButton();
-      buttonCreated = true;
-    }
+    // appendChild
+    todoList.appendChild(div);
+    div.appendChild(li);
+    div.appendChild(span);
+    span.appendChild(icon);
+
+    // setAttribute
+    div.setAttribute("class", "todo");
+    li.setAttribute("class", "task");
+    span.setAttribute("class", "delete btn");
+    icon.setAttribute("class", "fas fa-trash-alt");
   }
   input.value = "";
 }
 
-// Create remove button
-function newButton() {
-  const button = document.createElement("button");
-  const i = document.createElement("i");
-  removeBtn.appendChild(button);
-  button.setAttribute("class", "btn btn-width");
-  button.appendChild(i);
-  i.setAttribute("class", "fas fa-minus-square");
-}
+// FIX GladeFace Funtion !!!!
 
 function gladFace() {
   if (!angryFace.hasAttribute("fa-angry")) {
@@ -49,3 +47,5 @@ function gladFace() {
     angryFace.classList.add("fa-angry");
   }
 }
+
+// DELETE Function !!!!
