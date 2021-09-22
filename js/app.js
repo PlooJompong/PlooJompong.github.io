6a379fecf96cae9472baf023809c233ef1868942
Ploo Jompong <50630228+ponjom2@users.noreply.github.com>
2021-09-22 15:22:20 +0200

    Finish delete button

--- js/app.js
+++ js/app.js
@@ -2,48 +2,50 @@
 const input = document.getElementById("input");
 const addBtn = document.getElementById("add-button");
 const todoList = document.getElementById("todo-list");
-// const removeBtn = document.getElementById("remove-btn")
 const angryFace = document.getElementById("angry-face");
 
 // EventListeners
 addBtn.addEventListener("click", newTodo);
-// removeBtn.addEventListener("click", removeTask);
+todoList.addEventListener("click", deleteTask);
 angryFace.addEventListener("mouseenter", gladFace);
 
+
 // Functions for EventListener
-function newTodo(e) {
-  e.preventDefault();
-
+function newTodo() {
+  
   // Add listitem
-  const li = document.createElement("li");
-  const span = document.createElement("span");
-  const div = document.createElement("div");
-  const icon = document.createElement("i");
   if (input.value != "") {
+    
+    // CreateElements
+    const div = document.createElement("div");
+    const li = document.createElement("li");
+    const span = document.createElement("span");
+    
     li.innerText = input.value;
-
+    
     // appendChild
     todoList.appendChild(div);
     div.appendChild(li);
     div.appendChild(span);
-    span.appendChild(icon);
-
+    span.innerHTML = '<i class="fas fa-trash-alt"></i>';
+    
     // setAttribute
-    div.setAttribute("class", "todo");
+    div.setAttribute("id", "todo");
     li.setAttribute("class", "task");
     span.setAttribute("id", "remove-btn");
-    span.setAttribute("class", "delete btn");
-    icon.setAttribute("class", "fas fa-trash-alt");
+    span.setAttribute("class", "delete-btn btn");
   }
   input.value = "";
 }
 
-// Remove Button !!!
-/*
-function removeTask() {
-  console.log("hej");
+// Delete Button
+function deleteTask(e) {
+  const item = e.target;
+  if (item.classList[0] === "delete-btn") {
+    const todo = item.parentElement;
+    todo.remove();
+    }
 }
-*/
 
 // FIX GladeFace Funtion !!!!
 
@@ -52,7 +54,6 @@ function gladFace() {
     angryFace.classList.remove("fa-angry");
     angryFace.classList.add("fa-smile-beam");
   } else {
-    angryFace.classList.remove("fa-smile-beam");
-    angryFace.classList.add("fa-angry");
-  }
+      angryFace.classList.remove("fa-smile-beam");
+    }
 }
